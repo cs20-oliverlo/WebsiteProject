03b52f07a6150aa74a4c45adbf3a5f573bab9e9a
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-09-21 11:21:52 -0600

    awd

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
 // Weapon Tabs
-function openWeapon(evt, weaponName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(weaponName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
+let tabs = document.querySelectorAll(".tabs button");
+let tabContents = document.querySelectorAll(".tab-content div");
+
+tabs.forEach((tab, index) => {
+  tab.addEventListener("click", () => {
+    tabContents.forEach((content) => {
+      content.classList.remove("tabcontent");
+    });
+    tabs.forEach((tab) => {
+      tab.classList.remove("tabcontent");
+    });
+    tabContents[index].classList.add("tabcontent");
+    tabs[index].classList.add("tabcontent");
+  });
+});
 
   // Quizizzito
   
