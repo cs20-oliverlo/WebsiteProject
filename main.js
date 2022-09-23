b78a35b5bcf6dfd63377ee4f96558c7b6cf1eade
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-09-23 17:53:08 -0600

    :(

--- main.js
+++ main.js
@@ -1,21 +1,18 @@
-// Weapon TabsweaponName
-let tablinks = document.getElementsByClassName("tablinks")
-for (let i = 0; i < tablinks.length; i++) {
-  tablinks[i].addEventListener("click", tabFunc);
-}
+  // Tabs
+  let tabs = document.querySelectorAll(".tabs button");
+  let tabContents = document.querySelectorAll(".tabstuff div");
 
-function tabFunc() {
-    // Get all elements with class="tablinks" and remove the class "active"
-    let tablinks = document.querySelectorAll(".tablinks button");
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(weaponName).style.display = "block";
-    evt.currentTarget.className += " active";
-}
+  tabs.forEach((tab, index) => {
+    tab.addEventListener("click", () => {
+      tabContents.forEach((content) => {
+        content.classList.remove("active");
+      });
+      tabs.forEach((tab) => {
+        tab.classList.remove("active");
+      });
+      tabContents[index].classList.add("active");
+      tabs[index].classList.add("active");
+    });
+  });
 
   // Quizizzito
-  
