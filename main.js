c7afcd288107568903a391b42fa91e05ad57a4d3
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-09-26 10:40:29 -0600

    gawk gawk

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-// Weapon TabsweaponName
+//New version of tabs on about page that uses event listeners
 let tablinks = document.getElementsByClassName("tablinks")
 for (let i = 0; i < tablinks.length; i++) {
-  tablinks[i].addEventListener("click", tabFunc);
+  tablinks[i].addEventListener("click", function() {newChar(i);});
 }
 
-function tabFunc() {
-    // Get all elements with class="tablinks" and remove the class "active"
-    let tablinks = document.querySelectorAll(".tablinks button");
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(weaponName).style.display = "block";
-    evt.currentTarget.className += " active";
+function newChar(tabValue) {
+  // Get all elements with class="tabcontent" and hide them
+  let tabcontent = document.getElementsByClassName("tabcontent");
+  for (let i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Show the current tab
+  let tabName = tablinks[tabValue].innerHTML;
+  document.getElementById(tabName).style.display = "block";
 }
 
   // Quizizzito
-  
