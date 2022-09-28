19590df9966e642a52171260b57977a9c255211a
oliverlo1 <113052541+oliverlo1@users.noreply.github.com>
2022-09-27 20:56:45 -0600

    awd

--- main.js
+++ main.js
@@ -15,4 +15,27 @@
     });
   });
 
-  // Quizizzito
+// Quizizzito
+
+// HTML Variables
+let testScore = +document.getElementById("answers-right");
+
+// Button Event Listener
+document.getElementById("checkButton").addEventListener("click", btnClicked);
+
+function btnClicked() {
+  // Input
+  let answer1 = document.getElementById("ans1-in").value.toLowerCase();
+  let answer2 = document.getElementById("ans2-in").value.toLowerCase();
+  let answer3 = document.getElementById("ans3-in").value.toLowerCase();
+  let answer4 = document.getElementById("ans4-in").value.toLowerCase();
+  let answer5 = document.getElementById("ans5-in").value.toLowerCase();
+  let answer6 = document.getElementById("ans6-in").value.toLowerCase();
+
+  // If Statement - Test the input
+  if (answer1 === "hakita" || answer1 === "pitr") {
+    testScore;
+  } else () {
+
+  }
+}
